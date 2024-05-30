43b9d24c2e5065c015f45f054ab2d263b90ff569
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-05-30 08:28:14 +0800

    what if mo undang nta

--- assets/js/accounting.js
+++ assets/js/accounting.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent form submission
 
+        var usernameInput = document.getElementById('username');
+        var passwordInput = document.getElementById('password');
+        var username = usernameInput.value.trim().toLowerCase();
+        var password = passwordInput.value.trim().toLowerCase();
+
+        if (username === "admin" && password === "admin") {
+            window.location.href = "dashboard.php";
+            alert('Welcome, admin!');
+        } else {
+            alert('Incorrect username or password!');
+        }
+
+        // Clear input fields after login attempt
+        usernameInput.value = '';
+        passwordInput.value = '';
+    });
+});
