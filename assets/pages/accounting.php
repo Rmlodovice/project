fa3b607f690eb008c59a1b3ec0eb95789a785018
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-05-30 08:29:10 +0800

    laaya oiii

--- assets/pages/accounting.php
+++ assets/pages/accounting.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="../css/accounting.css">
+    
+</head>
+<body>
+    <div id="form" method="post">
+        <form action="" >
+            <label>Username:</label><br>
+            <input type="text" id="username" name="username" required><br>
+            <label>Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <button id="loginButton">Login</button>
+        </form>
+    </div>
+    <script src="../js/accounting.js"></script>
+</body>
+</html>
---
+++ assets/pages/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <style>
+        h1 {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+<body>
+    <H1>Dashboard<br>BATI KAG NAWONG!</H1>
+</body>
+</html>
