437d3aa35c6468b9fdb070fc97f599c455ba842d
earltyront <151801269+earltyront@users.noreply.github.com>
2024-05-31 23:42:16 -0400

    Accounting modified

--- assets/pages/accounting.php
+++ assets/pages/accounting.php
@@ -1,19 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
+    <title>Receipt Tracker</title>
     <link rel="stylesheet" href="../css/accounting.css">
-    
 </head>
 <body>
-    <div id="form" method="post">
-        <form action="" >
-            <label>Username:</label><br>
-            <input type="text" id="username" name="username" required><br>
-            <label>Password:</label><br>
-            <input type="password" id="password" name="password" required><br><br>
-            <button id="loginButton">Login</button>
-        </form>
+    <header class="headerClass">
+        <div class="headerR">
+            <h1> Receipt Tracker <h1>
+            <a href="newAccount.php" class="addUser">Add User</a>
+            <a href="addReceipt.php" class="addReceipt">Add Receipt</a>
+        </div>
+    </header>
+    <div class="accounting">
+        <h2>Welcome, Accounting!</h2>
+        <div><p>Add account now! <a href="newAccount.php">Click Here.</a></p></div>
+        <footer>
+            <a href="logout.php" class="btn btn-danger">Logout</a>
+        </footer>
     </div>
     <script src="../js/accounting.js"></script>
 </body>
