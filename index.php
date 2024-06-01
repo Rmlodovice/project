c8becaab8bd6b11b9346c9ebcbebf80df082b713
Rmlodovice <150519673+Rmlodovice@users.noreply.github.com>
2024-05-31 23:47:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Page</title>
-    <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="icon" href="assets/image/iconTabLogo.png" type="image/png">
-    
-</head>
-<body>
-    
-    <h1>Welcome to Accounting</h1>
-    <div id="form" method="post">
-        <form action="" >
-            <label>Username:</label><br>
-            <input type="text" id="username" name="username" required><br>
-            <label>Password:</label><br>
-            <input type="password" id="password" name="password" required><br><br>
-            <button id="loginButton">Login</button>
-        </form>
-    </div>
-    <script src="assets/js/accounting.js"></script>
-</body>
-</html>
+<?php
+ header("Location: login/login.php")
+?>
